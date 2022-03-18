53351cfbde039a0b84c378584708d1985c9b7857
Osaze Oviawe <ocubed88@gmail.com>
2022-03-18 13:45:34 +0100

    feat: - added ability to switch theme from light to dark mode

--- app/sections/Header.jsx
+++ app/sections/Header.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import Logo from '../components/Logo';
+import { useTheme } from 'next-themes';
+import {MoonIcon, SunIcon} from '@heroicons/react/solid';
 
 export default function Header() {
+    const {systemTheme, theme, setTheme} = useTheme();
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => {
+        setMounted(true)
+    }, [])
+
+    const changeTheme = () => {
+        if (!mounted) return null;
+
+        const currentTheme = theme === 'system' ? systemTheme : theme
+
+        if (currentTheme === 'dark') {
+            return (
+                <SunIcon className='w-7 h-7' role="button" onClick={() => setTheme('light')} />
+            )
+        } else {
+            return (
+                <MoonIcon className='w-7 h-7' role="button" onClick={() => setTheme('dark')} />
+            )
+        }
+    }
+
     return (
-        <header className='border-b border-gray-100'>
-            <div className='container mx-auto px-4 sm:px-6 py-4'>
+        <header className='border-b border-gray-100 dark:border-gray-700'>
+            <div className='container mx-auto px-4 sm:px-6 py-4 flex justify-between items-center'>
                 <Logo />
+
+                {changeTheme()}
             </div>
         </header>
     )
