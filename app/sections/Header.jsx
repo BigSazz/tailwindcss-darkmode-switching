2e6331f98ecef68136ef038894776e0ae22cd9d4
Osaze Oviawe <ocubed88@gmail.com>
2022-03-18 16:37:38 +0100

    feat: - added menu - added mobile menu - added signout functionality

--- app/sections/Header.jsx
+++ app/sections/Header.jsx
@@ -1,11 +1,20 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useState, useRef} from 'react'
 import Logo from '../components/Logo';
 import { useTheme } from 'next-themes';
+import { useSession, signIn } from 'next-auth/react'
 import {MoonIcon, SunIcon} from '@heroicons/react/solid';
+import FlyoutMenu from '../components/FlyoutMenu';
+import MobileMenu from '../components/MobileMenu';
+import { ChevronDownIcon } from '@heroicons/react/outline';
+import useMediaQuery from '../hooks/use-media-query';
 
 export default function Header() {
     const {systemTheme, theme, setTheme} = useTheme();
     const [mounted, setMounted] = useState(false);
+    const {data: session, status } = useSession();
+    const [menuOpen, setMenuOpen] = useState(false);
+    const containerRef = useRef();
+    const isLargeScreen = useMediaQuery(['(min-width: 640px'], [true], false)
 
     useEffect(() => {
         setMounted(true)
@@ -32,8 +41,40 @@ export default function Header() {
             <div className='container mx-auto px-4 sm:px-6 py-4 flex justify-between items-center'>
                 <Logo />
 
-                {changeTheme()}
+                <div className='flex space-x-4 justify-center items-center'>
+                    {changeTheme()}
+                    {status !== 'loading' ? (
+                        <div>
+                            {!session ? (
+                                <button
+                                    type='button'
+                                    onClick={signIn}
+                                    className='bg-blue-600 hover:bg-blue-700 text-white px-6 py-3 rounded-md border-2 border-blue-600 hover:border-blue-700 text-lg sm:text-xl focus:outline-none focus:ring-4 focus:ring-blue-600 focus:ring-opacity-50 whitespace-nowrap'
+                                >Sign in</button>
+                            ) : (
+                                <div className='relative' ref={containerRef}>
+                                    <button 
+                                        onClick={() => setMenuOpen(prev => !prev)}
+                                        className='flex items-center justify-center space-x-1 sm:space-x-2'>
+                                        {/* eslint-disable-next-line @next/next/no-img-element */}
+                                        <img className='rounded-full border-2 border-blue-600 w-8 h-8' src={session.user.image} alt={session.user.name} />
+                                        <p className='flex items-center sm:space-x-1'>
+                                            <span className='hidden sm:inline-block'>
+                                                Hello, {session.user.name?.split(" ")?.[0] ?? 'there'}
+                                            </span>
+                                            <ChevronDownIcon className='w-4 h-4 flex-shrink-0' />
+                                        </p>
+                                    </button>
+
+                                    <FlyoutMenu show={menuOpen && isLargeScreen} onClose={() => setMenuOpen(false)} containerRef={containerRef} />
+                                </div>
+                            )}
+                        </div>
+                    ) : null}
+                </div>
             </div>
+
+            <MobileMenu show={menuOpen && !isLargeScreen} onClose={() => setMenuOpen(false)} />
         </header>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
-import Image from 'next/image';
 import Layout from '../app/sections/Layout';
+import { useSession, signIn } from 'next-auth/react';
+import Link from 'next/link';
+import { PencilIcon } from '@heroicons/react/solid';
 
 export default function Home() {
+	const { data: session, status } = useSession();
+
 	return (
 		<div>
 			<Layout pageMeta={{ title: 'Home Page' }}>
@@ -20,13 +24,26 @@ export default function Home() {
 					</div>
 
 					{/* CTA */}
-					<button
-						type='button'
-						onClick={null}
-						className='bg-blue-600 hover:bg-blue-700 text-white px-6 py-3 rounded-md border-2 border-blue-600 hover:border-blue-700 text-lg sm:text-xl focus:outline-none focus:ring-4 focus:ring-blue-600 focus:ring-opacity-50 whitespace-nowrap'
-					>
-						Start your blog for free
-					</button>
+					{status === 'loading' ? null : (
+						<>
+							{!session ? (
+								<button
+									type='button'
+									onClick={signIn}
+									className='bg-blue-600 hover:bg-blue-700 text-white px-6 py-3 rounded-md border-2 border-blue-600 hover:border-blue-700 text-lg sm:text-xl focus:outline-none focus:ring-4 focus:ring-blue-600 focus:ring-opacity-50 whitespace-nowrap'
+								>
+									Start your blog for free
+								</button>
+							) : (
+								<Link href='/'>
+									<a className='bg-blue-600 hover:bg-blue-700 text-white px-6 py-3 rounded-md border-2 border-blue-600 hover:border-blue-700 text-lg sm:text-xl focus:outline-none focus:ring-4 focus:ring-blue-600 focus:ring-opacity-50 whitespace-nowrap flex justify-center items-center space-x-2'>
+										<PencilIcon className='w-6 h-6 flex-shrink-0' />
+										<span>Write a blog post</span>
+									</a>
+								</Link>
+							)}
+						</>
+					)}
 				</section>
 			</Layout>
 		</div>
